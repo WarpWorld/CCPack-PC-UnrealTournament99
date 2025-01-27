9dcfa0ec14c47e2cdcc54d035cde1029dc08ac35
MrS4turn <67934254+MrS4turn@users.noreply.github.com>
2025-01-27 14:10:28 -0500

    dev sync

--- UnrealTournament99.cs
+++ UnrealTournament99.cs
@@ -50,7 +50,7 @@ public class UnrealTournament99 : SimpleTCPPack<SimpleTCPServerConnector>
         new("Just Skin and Bones", "skin_and_bones"){Price = 4, Description = "All players become extremely skinny!", Duration=60},
         new("Gotta Go Fast", "gotta_go_fast"){Price = 10, Description = "It's extra fast mode!", Duration=60},
         new("Gotta Go Slow", "gotta_go_slow"){Price = 10, Description = "It's extra slow mode!", Duration=15},
-        new("Swap Two Players Positions", "swap_player_position"){Price = 10, Description = "2 players swap positions on the map!"},
+        new("Swap All Players Positions", "swap_player_position"){Price = 10, Description = "All players swap positions with other players on the map!"},
         new("Nudge All Players", "nudge"){Price = 5, Description = "Push the players around!"},
         new("Ice Physics", "ice_physics"){Price = 10, Description = "Summon frosty floors!", Duration=60},
         new("Low Gravity", "low_grav"){Price = 5, Description = "Low gravity means players jump higher!", Duration=60},
@@ -60,10 +60,15 @@ public class UnrealTournament99 : SimpleTCPPack<SimpleTCPServerConnector>
         new("Spawn a Defending Bot (One Death)", "spawn_a_bot_defend"){Price = 10, Description = "This will spawn a bot on whatever team is has the least amount of players with orders to defend their base"},
         new("Reset Domination Control Points", "reset_domination_control_points"){Price = 5, Description = "This will reset all control points in Domination Mode to neutral"},
         new("Return Flags", "return_ctf_flags"){Price = 5, Description = "In Capture the Flag Mode, this will return all flags to their base"},
-        
+        new("Jump Boot Madness", "jump_boot_madness"){Price = 3, Description = "Give all players jump boots and keep them topped up so they can jump really high!", Duration=60},
+        new("Massive Momentum", "massive_momentum"){Price = 3, Description = "All damage imparts significantly more momentum to the target!", Duration=60},
+        new("Bouncy Castle", "bouncy_castle"){Price = 3, Description = "Everyone gets periodically bounced up into the air!", Duration=60},
+        new("Red Light, Green Light", "red_light_green_light"){Price = 10, Description = "The light randomly changes between Red and Green!  Move while the light is red and... KABOOM!", Duration=60},
+
+
         ////////////////////////////////////////////////////////////////
         
-        //new Effect("Health and Armor","health",ItemKind.Folder),
+        //new("Health and Armor","health",ItemKind.Folder),
         new("Full Heal", "full_heal") { Category = "Health & Ammo", Price = 5, Description = "Send a full heal to all players!" },
         new("Shield Belts for All", "full_armour") { Category = "Health & Ammo", Price = 5, Description = "You get a shield belt and you get a shield belt! Everyone gets shield belts!" },
         new("Give Health", "give_health")
@@ -74,14 +79,14 @@ public class UnrealTournament99 : SimpleTCPPack<SimpleTCPServerConnector>
             Description = "Give a little health!"
         },
         new("Sudden Death", "sudden_death") { Category = "Health & Ammo", Price = 10, Description = "Activate sudden death mode!" },
-        new("Thanos Snap", "thanos") { Category = "Health & Ammo", Price = 15, Description = "Each player has a 50% chance of instantly being killed!" },
+        new("Thanos Snap", "thanos") { Category = "Health & Ammo", Price = 15, Description = "Half of the players in the match get obliterated!" },
         new("Vampiric Attacks", "vampire_mode") { Category = "Health & Ammo", Price = 10, Description = "All attacks by players sap some life, healing the player!", Duration=60 },
         new("Give Shield Belt to Last Place", "last_place_shield") { Category = "Health & Ammo", Price = 5, Description = "Help out that last place player!" },
         new("Blue (Redeemer) Shell", "blue_redeemer_shell") { Category = "Health & Ammo", Price = 15, Description = "Drops a redeemer explosion on the player in first place!" },
         
         /////////////////////////////////////////////////////////////////
         
-        //new Effect("Weapons and Damage","weapons",ItemKind.Folder),
+        //new("Weapons and Damage","weapons",ItemKind.Folder),
         new("Give Weapon to All", "give_weapon")
         {
             Parameters = weaponList,
@@ -113,6 +118,8 @@ public class UnrealTournament99 : SimpleTCPPack<SimpleTCPServerConnector>
         new("Bonus Damage for All", "bonus_dmg") { Category = "Weapons & Damage", Price = 5, Description = "Pump up the damage on all players" },
         new("Melee Only!", "melee_only") { Category = "Weapons & Damage", Price = 10, Description = "Never mind these guns, it's punching time!", Duration=60 },
         new("Bonus Damage for Last Place", "last_place_bonus_dmg") { Category = "Weapons & Damage", Price = 5, Description = "Help out the last place player and grant them bonus damage!" },
-        new("All Players Drop Current Weapon", "drop_selected_item") { Category = "Weapons & Damage", Price = 10, Description = "Who needs this weapon anyway..." }
+        new("All Players Drop Current Weapon", "drop_selected_item") { Category = "Weapons & Damage", Price = 10, Description = "Who needs this weapon anyway..." },
+        new("Thorns", "thorns") { Category = "Weapons & Damage", Price = 5, Description = "All players grow thorns, inflicting damage back on those who are dealing damage.", Duration=60},
+        new("Weapon Chaos", "random_weapon_swap") { Category = "Weapons & Damage", Price = 5, Description = "All players start randomly switching weapons!", Duration=60}
     };
 }
